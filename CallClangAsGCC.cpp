bc221c004f2f221f8af3c51703e75ef188ff06ee
Maulana Muhammad Alghifary <38803815+AlGamersAI@users.noreply.github.com>
2020-04-17 22:15:17 +0700

    Create CallClangAsGCC.cpp

---
+++ CallClangAsGCC.cpp
@@ -0,0 +1,10 @@
+// Call Clang as GCC
+// By Maulana Muhammad Alghifary
+// This file is licensed under Mozilla Public License 2.0. Act of any kinds will be under this licence.
+
+#include <iostream>
+#include <string>
+#include "main.h"
+
+// Definitions
+void CallClangAsGCC::
