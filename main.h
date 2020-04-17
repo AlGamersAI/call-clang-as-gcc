9dc3597d454f914553408bba6afc72380995da0e
Maulana Muhammad Alghifary <38803815+AlGamersAI@users.noreply.github.com>
2020-04-17 22:10:46 +0700

    Create main.h header

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+// We are starting to define this header
+// By Maulana Muhammad Alghifary
+
+class CallClangAsGCC{
+public:
+  CallClangAsGCC();
+  ~CallClangAsGCC();
+  void MainFunction(char **Arguments);
+};
+
+#endif // MAIN_H_
